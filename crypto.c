182d32ee45330f74933d291dd1d1e97aacd966f1
Franz Brauße <brausse@informatik.uni-trier.de>
2019-08-21 12:22:10 +0200

    remove old just-gcrypt-based versions of decrypt_envelope() and verify_envelope()

--- crypto.c
+++ crypto.c
@@ -32,7 +32,6 @@ static bool verify_envelope(const uint8_t *body, size_t *size_ptr,
 	if (size < MAC_SIZE + 1)
 		return false;
 
-#if 1
 	void *ctx;
 	if (crypto_provider.hmac_sha256_init_func(&ctx, mac_key, MAC_KEY_SIZE, NULL))
 		FAIL(fail, "error on hmac_sha256_init_func\n");
@@ -70,40 +69,6 @@ mac_fail_2:
 	signal_buffer_free(our_mac);
 mac_fail:
 	crypto_provider.hmac_sha256_cleanup_func(ctx, NULL);
-#else
-	gcry_md_hd_t hd;
-	gcry_error_t gr;
-	gr = gcry_md_open(&hd, GCRY_MD_SHA256, GCRY_MD_FLAG_HMAC);
-	if (gr)
-		FAIL(fail, "error on gcry_md_open: %x\n", gr);
-	gr = gcry_md_setkey(hd, mac_key, MAC_KEY_SIZE);
-	if (gr)
-		FAIL(mac_fail, "error on gcry_md_setkey: %x\n", gr);
-	gcry_md_write(hd, body, size - MAC_SIZE);
-	gcry_md_final(hd);
-	const uint8_t *our_mac = gcry_md_read(hd, GCRY_MD_SHA256);
-	assert(our_mac);
-	const uint8_t *their_mac = body + size - MAC_SIZE;
-	if (memcmp(our_mac, their_mac, MAC_SIZE)) {
-		fprintf(stderr, "MACs don't match:\n");
-		fprintf(stderr, "  ours  : ");
-		print_hex(stderr, our_mac, MAC_SIZE);
-		fprintf(stderr, "\n  theirs: ");
-		print_hex(stderr, their_mac, MAC_SIZE);
-		fprintf(stderr, "\n");
-		goto mac_fail;
-	}
-	gcry_md_close(hd);
-
-	// fprintf(stderr, "MACs match! :)\n");
-
-	size -= MAC_SIZE;
-	*size_ptr = size;
-	return true;
-
-mac_fail:
-	gcry_md_close(hd);
-#endif
 fail:
 	return false;
 }
@@ -114,24 +79,24 @@ fail:
  *       SIGNALING_KEY = CIPHER_KEY MAC_KEY
  */
 bool decrypt_envelope(uint8_t **body_ptr, size_t *size_ptr,
-                      const char *signaling_key_base64,
-                      size_t signaling_key_base64_len)
+                      const char *sg_key_b64,
+                      size_t sg_key_b64_len)
 {
 	uint8_t *body = *body_ptr;
 	size_t size = *size_ptr;
 
 	if (size < VERSION_LENGTH || body[VERSION_OFFSET] != VERSION_SUPPORTED)
 		return false;
-	assert(signaling_key_base64);
+	assert(sg_key_b64);
 	/* properties of base64 */
-	assert(signaling_key_base64_len >= (4*(CIPHER_KEY_SIZE + MAC_KEY_SIZE) + 2) / 3);
-	assert(signaling_key_base64_len <= (4*(CIPHER_KEY_SIZE + MAC_KEY_SIZE + 2)) / 3);
+	assert(sg_key_b64_len >= (4*(CIPHER_KEY_SIZE + MAC_KEY_SIZE) + 2) / 3);
+	assert(sg_key_b64_len <= (4*(CIPHER_KEY_SIZE + MAC_KEY_SIZE + 2)) / 3);
 	uint8_t decoded_key[CIPHER_KEY_SIZE + MAC_KEY_SIZE];
-	ssize_t r = base64_decode(decoded_key, signaling_key_base64, signaling_key_base64_len);
+	ssize_t r = base64_decode(decoded_key, sg_key_b64, sg_key_b64_len);
 	if (r != CIPHER_KEY_SIZE + MAC_KEY_SIZE) {
 		fprintf(stderr,
 		        "error decoding signalingKey of length %zu: r: %zd\n",
-		        signaling_key_base64_len, r);
+		        sg_key_b64_len, r);
 		return false;
 	}
 	const uint8_t *cipher_key = decoded_key;
@@ -143,7 +108,6 @@ bool decrypt_envelope(uint8_t **body_ptr, size_t *size_ptr,
 	body += VERSION_LENGTH;
 
 	/* decode AES/CBC/PKCS5Padding */
-#if 1
 	signal_buffer *plaintext = NULL;
 	uint8_t *iv = body;
 	body += IV_LENGTH;
@@ -157,53 +121,6 @@ bool decrypt_envelope(uint8_t **body_ptr, size_t *size_ptr,
 	*body_ptr = body;
 	signal_buffer_free(plaintext);
 	return true;
-#else
-	gcry_cipher_hd_t ci;
-	gcry_error_t gr;
-	gr = gcry_cipher_open(&ci, GCRY_CIPHER_AES256, GCRY_CIPHER_MODE_CBC, 0);
-	if (gr)
-		FAIL(fail, "error on gcry_cipher_open: %x\n", gr);
-	gr = gcry_cipher_setkey(ci, cipher_key, CIPHER_KEY_SIZE);
-	if (gr)
-		FAIL(cipher_fail, "error on gcry_cipher_setkey: %x\n", gr);
-	gr = gcry_cipher_setiv(ci, body, IV_LENGTH);
-	if (gr)
-		FAIL(cipher_fail, "error on gcry_cipher_setiv: %x\n", gr);
-	size -= IV_LENGTH;
-	body += IV_LENGTH;
-	gr = gcry_cipher_final(ci);
-	if (gr)
-		FAIL(cipher_fail, "error on gcry_cipher_final: %x\n", gr);
-	gr = gcry_cipher_decrypt(ci, body, size, NULL, 0);
-	if (gr)
-		FAIL(cipher_fail, "error on gcry_cipher_decrypt: %x\n", gr);
-	gcry_cipher_close(ci);
-
-	/* remove PKCS5Padding */
-	if (!size)
-		FAIL(fail, "size of decrypted envelope is zero\n");
-	unsigned n = body[size-1];
-	if (size < n)
-		FAIL(fail, "size of decrypted envelope is smaller than "
-		           "PKCS5Padding's value\n");
-	for (unsigned i=0; i<n; i++)
-		if (body[size-n+i] != n)
-			FAIL(fail,
-			     "PKCS5Padding of decrypted envelope is broken\n");
-	size -= n;
-
-	/*
-	fprintf(stderr, "success: ");
-	print_hex(stderr, body, size);
-	fprintf(stderr, "\n");*/
-
-	*body_ptr = body;
-	*size_ptr = size;
-	return true;
-
-cipher_fail:
-	gcry_cipher_close(ci);
-#endif
 fail:
 	return false;
 }
